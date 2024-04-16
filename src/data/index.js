0acf044116d75cff5239791720942d59a19c6ebc
XenoZ <xenoz0096@gmail.com>
2024-04-16 19:43:51 +0530

    first commit

---
+++ src/data/index.js
@@ -0,0 +1,316 @@
+import { faker } from "@faker-js/faker";
+import {
+  ChatCircleDots,
+  Gear,
+  GearSix,
+  Phone,
+  SignOut,
+  User,
+  Users,
+} from "phosphor-react";
+
+const Profile_Menu = [
+  {
+    title: "Profile",
+    icon: <User />,
+  },
+  {
+    title: "Settings",
+    icon: <Gear />,
+  },
+  {
+    title: "Logout",
+    icon: <SignOut />,
+  },
+];
+
+const Nav_Buttons = [
+  {
+    index: 0,
+    icon: <ChatCircleDots />,
+  },
+  {
+    index: 1,
+    icon: <Users />,
+  },
+  {
+    index: 2,
+    icon: <Phone />,
+  },
+];
+
+const Nav_Setting = [
+  {
+    index: 3,
+    icon: <GearSix />,
+  },
+];
+
+const CallLogs = [
+  {
+    id:0,
+    img: faker.image.avatar(),
+    name: "Alan",
+    missed: false,
+    incoming: true,
+  },
+
+];
+
+const MembersList = [
+  {
+    id:0,
+    img: faker.image.avatar(),
+    name: "Alan",
+    online: true
+  },
+  {
+    id:1,
+    img: faker.image.avatar(),
+    name: "Bob",
+    online: true
+  },
+  {
+    id:2,
+    img: faker.image.avatar(),
+    name: "Carol",
+    online: true
+  },
+  {
+    id:3,
+    img: faker.image.avatar(),
+    name: "Dean",
+    online: true
+  },
+  {
+    id:4,
+    img: faker.image.avatar(),
+    name: "Elin",
+    online: true
+  },
+ 
+];
+
+const ChatList = [
+  {
+    id: 0,
+    img: faker.image.avatar(),
+    name: "Alan",
+    msg: faker.music.songName(),
+    time: "9:36",
+    unread: 0,
+    pinned: true,
+    online: true,
+  },
+  {
+    id: 1,
+    img: faker.image.avatar(),
+    name: "Bob",
+    msg: faker.music.songName(),
+    time: "5:14",
+    unread: 0,
+    pinned: true,
+    online: false,
+  },
+  {
+    id: 2,
+    img: faker.image.avatar(),
+    name: "Carol",
+    msg: faker.music.songName(),
+    time: "5:12",
+    unread: 0,
+    pinned: true,
+    online: false,
+  },
+  {
+    id: 3,
+    img: faker.image.avatar(),
+    name: "Dean",
+    msg: faker.music.songName(),
+    time: "12:11",
+    unread: 0,
+    pinned: true,
+    online: false,
+  },
+  {
+    id: 4,
+    img: faker.image.avatar(),
+    name: "Elin",
+    msg: faker.music.songName(),
+    time: "7:19",
+    unread: 0,
+    pinned: true,
+    online: false,
+  },
+];
+
+const Chat_History = [
+  {
+    type: "msg",
+    message: "Hi 👋🏻, How are ya ?",
+    incoming: true,
+    outgoing: false,
+  },
+  {
+    type: "divider",
+    text: "Today",
+  },
+  {
+    type: "msg",
+    message: "Hi 👋 Panda, not bad, u ?",
+    incoming: false,
+    outgoing: true,
+  },
+  {
+    type: "msg",
+    message: "Can you send me an abstarct image?",
+    incoming: false,
+    outgoing: true,
+  },
+  {
+    type: "msg",
+    message: "Ya sure, sending you a pic",
+    incoming: true,
+    outgoing: false,
+  },
+
+  {
+    type: "msg",
+    subtype: "img",
+    message: "Here You Go",
+    img: faker.image.abstract(),
+    incoming: true,
+    outgoing: false,
+  },
+  {
+    type: "msg",
+    message: "Can you please send this in file format?",
+    incoming: false,
+    outgoing: true,
+  },
+
+  {
+    type: "msg",
+    subtype: "doc",
+    message: "Yes sure, here you go.",
+    incoming: true,
+    outgoing: false,
+  },
+  {
+    type: "msg",
+    subtype: "link",
+    preview: faker.image.cats(),
+    message: "Yep, I can also do that",
+    incoming: true,
+    outgoing: false,
+  },
+  {
+    type: "msg",
+    subtype: "reply",
+    reply: "This is a reply",
+    message: "Yep, I can also do that",
+    incoming: false,
+    outgoing: true,
+  },
+];
+
+const Message_options = [
+  {
+    title: "Reply",
+  },
+  {
+    title: "React to message",
+  },
+  {
+    title: "Forward message",
+  },
+  {
+    title: "Star message",
+  },
+  {
+    title: "Report",
+  },
+  {
+    title: "Delete Message",
+  },
+];
+
+const SHARED_LINKS = [
+  {
+    type: "msg",
+    subtype: "link",
+    preview: faker.image.cats(),
+    message: "Yep, I can also do that",
+    incoming: true,
+    outgoing: false,
+  },
+  {
+    type: "msg",
+    subtype: "link",
+    preview: faker.image.cats(),
+    message: "Yep, I can also do that",
+    incoming: true,
+    outgoing: false,
+  },
+  {
+    type: "msg",
+    subtype: "link",
+    preview: faker.image.cats(),
+    message: "Yep, I can also do that",
+    incoming: true,
+    outgoing: false,
+  },
+  {
+    type: "msg",
+    subtype: "link",
+    preview: faker.image.cats(),
+    message: "Yep, I can also do that",
+    incoming: true,
+    outgoing: false,
+  }
+]
+
+const SHARED_DOCS = [
+  {
+    type: "msg",
+    subtype: "doc",
+    message: "Yes sure, here you go.",
+    incoming: true,
+    outgoing: false,
+  },
+  {
+    type: "msg",
+    subtype: "doc",
+    message: "Yes sure, here you go.",
+    incoming: true,
+    outgoing: false,
+  },
+  {
+    type: "msg",
+    subtype: "doc",
+    message: "Yes sure, here you go.",
+    incoming: true,
+    outgoing: false,
+  },
+  {
+    type: "msg",
+    subtype: "doc",
+    message: "Yes sure, here you go.",
+    incoming: true,
+    outgoing: false,
+  },
+ 
+]
+
+export {
+  Profile_Menu,
+  Nav_Setting,
+  Nav_Buttons,
+  ChatList,
+  Chat_History,
+  Message_options,
+  SHARED_DOCS,
+  SHARED_LINKS,
+  CallLogs,
+  MembersList
+};
